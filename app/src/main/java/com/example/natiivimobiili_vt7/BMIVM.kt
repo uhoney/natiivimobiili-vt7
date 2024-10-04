60ad8d5a63e3fd12ee6d5d759f933407071345c5
uhoney <t2maju06@students.oamk.fi>
2024-10-04 12:51:01 +0300

    viikkotehtävä 7 valmis

--- app/src/main/java/com/example/natiivimobiili_vt7/BMIVM.kt
+++ app/src/main/java/com/example/natiivimobiili_vt7/BMIVM.kt
@@ -1,5 +1,48 @@
 package com.example.natiivimobiili_vt7
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
-class BMIVM : ViewModel()
+class BMIVM : ViewModel() {
+    var heightInput by mutableStateOf("")
+    var weightInput by mutableStateOf("")
+
+    private val height: Int
+        get() {
+            return heightInput.toIntOrNull() ?: 0
+        }
+
+    private val weight: Float
+        get() {
+            return weightInput.toFloatOrNull() ?: 0.0f
+        }
+
+    val bmiValue: Float
+        get() = calculateBmi()
+
+
+    // Muulla tavalla ei oikei meinannu lähteä toimimaan kuin näin.
+    // Halusin nimenomaan logiikan tänne, enkä mainactivityyn.
+    // HUOM! R.string.x EI ole string, vaan Integer
+    val bmiExplanation: Int
+        get() = when {
+            bmiValue == 0.0f -> R.string.none
+            bmiValue < 17 -> R.string.ref_severe_under
+            bmiValue < 18.5 -> R.string.ref_under
+            bmiValue < 25 -> R.string.ref_normal
+            bmiValue < 30 -> R.string.ref_fat1
+            bmiValue < 35 -> R.string.ref_fat2
+            bmiValue < 40 -> R.string.ref_fat3
+            else -> R.string.ref_fat4
+        }
+
+    private fun calculateBmi(): Float {
+        return if (weight > 0 && height > 0) {
+            (weight / ((height.toFloat() / 100) * (height.toFloat() / 100)))
+        } else {
+            0.0f
+        }
+    }
+}
--- app/src/main/java/com/example/natiivimobiili_vt7/MainActivity.kt
+++ app/src/main/java/com/example/natiivimobiili_vt7/MainActivity.kt
@@ -3,12 +3,25 @@ package com.example.natiivimobiili_vt7
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.natiivimobiili_vt7.ui.theme.Natiivimobiilivt7Theme
 
 class MainActivity : ComponentActivity() {
@@ -16,7 +29,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Natiivimobiilivt7Theme {
-                Surface(modifier = Modifier.systemBarsPadding()) {
+                Surface(
+                    modifier = Modifier.systemBarsPadding(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     BMIApp()
                 }
             }
@@ -25,10 +41,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BMIApp() {
-    Text(
-        "hello world"
-    )
+fun BMIApp(model: BMIVM = viewModel()) {
+
+    Column(
+        modifier = Modifier.padding(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            text = stringResource(R.string.body_mass_index),
+            fontSize = 24.sp,
+            color = MaterialTheme.colorScheme.primary,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp, bottom = 16.dp)
+        )
+        OutlinedTextField(
+            value = model.heightInput,
+            onValueChange = { model.heightInput = it.replace(',', '.') },
+            label = { Text(stringResource(R.string.height)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = model.weightInput,
+            onValueChange = { model.weightInput = it.replace(',', '.') },
+            label = { Text(stringResource(R.string.weight)) },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            text = stringResource(R.string.result, model.bmiValue)
+        )
+
+        Text(
+            // Kutsu pitää tehdä tuolla 'id' parametrilla
+            text = stringResource(id = model.bmiExplanation)
+        )
+        Text(
+            modifier = Modifier.padding(top = 26.dp),
+            text = "Disclaimer: BMI is not a substitute for professional medical advice.\nAlso, do not get upset by yo momma jokes"
+        )
+    }
 }
 
 
